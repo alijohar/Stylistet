c6c58879d3a8c7ad39fdb88d382b1ebcbe5bd7cb
ali johar <johar.ali1@gmail.com>
2023-05-11 07:18:34 +0330

    temp

--- app/src/main/java/com/amorphteam/stylistet/MainActivity.kt
+++ app/src/main/java/com/amorphteam/stylistet/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.amorphteam.stylistet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
